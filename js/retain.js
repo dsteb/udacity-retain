f4b9108c716333452c9a9d5231d29f177cc7d801
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-08-04 18:36:43 +0200

    feat: Initial commit
    The demonstration of MV* patterns.

---
+++ js/retain.js
@@ -0,0 +1,70 @@
+$(function(){
+  'use strict';
+
+    var model = {
+        init: function() {
+            if (!localStorage.notes) {
+                localStorage.notes = JSON.stringify([]);
+            }
+        },
+        add: function(obj) {
+            var data = JSON.parse(localStorage.notes);
+            data.push(obj);
+            localStorage.notes = JSON.stringify(data);
+        },
+        getAllNotes: function() {
+            return JSON.parse(localStorage.notes);
+        }
+    };
+
+
+    var octopus = {
+        addNewNote: function(noteStr) {
+            model.add({
+                content: noteStr,
+                date: Date.now()
+            });
+            view.render();
+        },
+
+        getNotes: function() {
+            return model.getAllNotes().reverse();
+        },
+
+        init: function() {
+            model.init();
+            view.init();
+        }
+    };
+
+
+    var view = {
+        init: function() {
+            this.noteList = $('#notes');
+            var newNoteForm = $('#new-note-form');
+            var newNoteContent = $('#new-note-content');
+            newNoteForm.submit(function(e){
+                octopus.addNewNote(newNoteContent.val());
+                newNoteContent.val('');
+                e.preventDefault();
+            });
+            view.render();
+        },
+        render: function(){
+            var htmlStr = '';
+            octopus.getNotes().forEach(function(note){
+                var date = new Date(note.date);
+                var dateString = date.toDateString() + ' ' + date.toTimeString().slice(0, 8);
+                htmlStr += '<li class="note">'+
+                        note.content +
+                        '<span class="note-date">' +
+                          dateString +
+                        '</span>' +
+                    '</li>';
+            });
+            this.noteList.html( htmlStr );
+        }
+    };
+
+    octopus.init();
+});
